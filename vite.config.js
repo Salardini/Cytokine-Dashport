b1f33f3da0d59136c926aa1bf9e326de34bdc0d2
Salardini <128343914+Salardini@users.noreply.github.com>
2025-03-30 23:11:15 -0500

    Update vite.config.js
    Signed-off-by: Salardini <128343914+Salardini@users.noreply.github.com>

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-<<<<<<< HEAD
     // Add the base path here:
     base: '/Cytokine-Dashport/',
-=======
-  base: '/Cytokine-Dashport/', // <-- Ensure this line is correct & file saved
->>>>>>> c239b240d6cdde7762f6186973786458690172c0
-=======
-  base: '/Cytokine-Dashport/', // <-- Ensure this line is correct & file saved 
->>>>>>> 67c00ee350541d083739dc2dbae8518da0619fc8
 })
